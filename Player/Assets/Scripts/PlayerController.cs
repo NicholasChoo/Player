16b5f0e3545b00c281562bb9ba363ede23f91d51
NicholasChoo <nchoojj1@gmail.com>
2020-10-28 18:47:26 +0800

    Update

--- Player/Assets/Scripts/PlayerController.cs
+++ Player/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed = 5f;
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
     private Vector2 moveVelocity;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float lookDirection;
 
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.S)) //down
+        {
+            lookDirection = 0f;
+        }
+        else if (Input.GetKey(KeyCode.W)) //up
+        {
+            lookDirection = 0.33f;
+        }
+        else if (Input.GetKey(KeyCode.A)) //left
+        {
+            lookDirection = 0.66f;
+        }
+        else if (Input.GetKey(KeyCode.D)) //right
+        {
+            lookDirection = 1f;
+        }
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Direction", lookDirection);
+
+        moveVelocity = movement.normalized * moveSpeed;
     }
 
     void FixedUpdate()
